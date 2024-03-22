5fa28259ba513835d01f7119dea1c53adfbfd193
rodio <rodionmarkelov01229@gmail.com>
2024-03-22 22:00:17 +0400

    docstring

--- lab_1/task1/encryption.py
+++ lab_1/task1/encryption.py
@@ -1,6 +1,14 @@
+import os
+
 import key_for_task1 as key
 
-def encryption(message: str):
+
+def encryption(message: str) -> str:
+    """
+    Функция шифрует переданное сообщение согласно "квадрату Полибия".
+    :param message:
+    :return str:
+    """
     message = message.lower()
     result = ""
     for letter in message:
@@ -11,12 +19,20 @@ def encryption(message: str):
             result += str(place_of_letter)
     return result
 
-def encryption_text(path: str):
+
+def encryption_text(path: str) -> None:
+    """
+    Функция считывает сообщение из переданного файла, шифрует его и записывает в новый файл.
+    :param path:
+    :return None:
+    """
+    os.walk("texts")
     with open(path, "r", encoding="UTF-8") as file1:
         text = " ".join(line.rstrip() for line in file1)
     encryption_text_str = encryption(text)
-    with open('texts/encryption_text.txt', 'w', encoding='utf-8') as file2:
+    with open('texts/task1_encryption_text.txt', 'w', encoding='utf-8') as file2:
         file2.write(encryption_text_str)
 
+
 if __name__ == "__main__":
-    encryption_text("texts/text_for_encryption.txt")
+    encryption_text("texts/task1_text_for_encryption.txt")
