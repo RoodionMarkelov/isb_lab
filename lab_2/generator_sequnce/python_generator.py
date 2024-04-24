9222641d4e6fd6b7f0a80e787b59ded8a5f4b475
rodio <rodionmarkelov01229@gmail.com>
2024-04-24 18:14:07 +0400

    code final

--- lab_2/generator_sequnce/python_generator.py
+++ lab_2/generator_sequnce/python_generator.py
@@ -1,16 +1,37 @@
 import random
+import sys
 
 
-def random_sequence():
-    result = ""
-    for i in range(0, 128):
-        result = result + str(random.randint(0, 1))
-    return result
+def random_sequence() -> str:
+    """
+    Функция генерирует два рандомных целых числа переводит их в битовый формат
+    и записывает результаты перевода в строку result. После строка возращается.
+    :return result(str):
+    """
+    try:
+        result = ""
+        for i in range(0, 2):
+            number = random.randint(0, sys.maxsize)
+            str_tmp = '{:b}'.format(number)
+            if len(str_tmp) < 64:
+                for j in range(0, 64 - len(str_tmp)):
+                    str_tmp = '0' + str_tmp
+            result = str_tmp + result
+        return result
+    except Exception as e:
+        print(f"Произошла ошибка: {e}")
 
 
-def main():
-    string = random_sequence()
-    print(string)
+def main() -> None:
+    """
+    Функция генерирует случайную последовательность и выводит её.
+    :return None:
+    """
+    try:
+        string = random_sequence()
+        print(string)
+    except Exception as e:
+        print(f"Произошла ошибка: {e}")
 
 
 if __name__ == "__main__":
--- lab_2/main.py
+++ lab_2/main.py
@@ -1,5 +1,6 @@
 import json
 import math
+import os
 
 from mpmath import gammainc
 from scipy.special import erfc
@@ -9,7 +10,7 @@ from constants import PATH, LENGTH_OF_BLOCK, PI_I
 def read_json_file(file_path: str) -> dict:
     """
     Функция считывает данные из JSON файла.
-    :param file_path:
+    :param file_path: указывает на расположение JSON файла.
     :return dict:
     """
     try:
@@ -25,72 +26,137 @@ def read_json_file(file_path: str) -> dict:
 
 
 def frequency_bitwise_test(sequence: str) -> float:
-    N = len(sequence)
-    sum = 0
-    for bit in sequence:
-        if bit == "0":
-            sum -= 1
-        else:
-            sum += 1
-    S_N = (1.0 / math.sqrt(N)) * sum
-    P_value = erfc(S_N)
-    return P_value
-
-
-def similar_test(sequence: str) -> float:
-    N = len(sequence)
-    sum = 0
-    for bit in sequence:
-        if bit == "1":
-            sum += 1
-    proportion_of_ones = sum / N
-    if abs(proportion_of_ones - (1 / 2)) < 2 / math.sqrt(N):
-        return 0
-    else:
-        V_n = 0
-        for i in range(0, N - 1):
-            if (sequence[i] != sequence[i + 1]):
-                V_n += 1
-        P_value = erfc(
-            abs(V_n - 2 * N * proportion_of_ones(1 - proportion_of_ones)) / (2 * math.sqrt(2 * N) * proportion_of_ones(
-                1 - proportion_of_ones)))
-        return P_value
-
-
-def longest_sequence_test(sequence: str):
-    blocks = []
-    for i in range(0, int(len(sequence) / LENGTH_OF_BLOCK)):
-        blocks.append(sequence[i * LENGTH_OF_BLOCK: (i + 1) * LENGTH_OF_BLOCK])
-    V = [0, 0, 0, 0]
-    for block in blocks:
-        count = 0
-        max_length = 0
-        for bit in block:
-            if bit == 1:
-                count += 1
-                max_length = max(max_length, count)
+    """
+    Функция принимает последовательность бит в виде строки,
+    а затем рассчитывает P-значение для частотного побитового теста
+    и возвращает его.
+    :param sequence: последовательность бит в виде строки(str).
+    :return float:
+    """
+    try:
+        N = len(sequence)
+        sum = 0
+        for bit in sequence:
+            if bit == "0":
+                sum -= 1
             else:
-                count = 0
-        if max_length <= 1:
-            V[0] += 1
-        if max_length == 2:
-            V[1] += 1
-        if max_length == 3:
-            V[2] += 1
-        if max_length >= 4:
-            V[3] += 1
-    Xi_in_2 = 0
-    for i in range(0, 4):
-        Xi_in_2 += pow((V[i] - 16 * PI_I[i]), 2) / (16 * PI_I[i])
-    P_value = gammainc(1.5, Xi_in_2 / 2)
+                sum += 1
+        S_N = (1.0 / math.sqrt(N)) * sum
+        P_value = erfc(S_N / math.sqrt(2))
+        if P_value < 0 or P_value > 1:
+            raise ValueError('P should be in range [0, 1]')
+        return P_value
+    except Exception as e:
+        print(f"Произошла ошибка: {e}")
+
     return P_value
 
 
-def main():
-    return
+def similar_sequences_test(sequence: str) -> float:
+    """
+    Функция принимает последовательность бит в виде строки,
+    а затем рассчитывает P-значение для теста на одинаковые подряд идущие биты
+    и возвращает его.
+    :param sequence: последовательность бит в виде строки(str).
+    :return float:
+    """
+    try:
+        N = len(sequence)
+        sum = 0
+        for bit in sequence:
+            if bit == "1":
+                sum += 1
+        proportion_of_ones = sum / N
+        if abs(proportion_of_ones - (1 / 2)) < 2 / math.sqrt(N):
+            V_n = 0
+            for i in range(0, N - 1):
+                if (sequence[i] != sequence[i + 1]):
+                    V_n += 1
+            P_value = erfc(
+                abs(V_n - 2 * N * proportion_of_ones * (1 - proportion_of_ones)) / (
+                        2 * math.sqrt(2 * N) * proportion_of_ones * (
+                        1 - proportion_of_ones)))
+            if P_value < 0 or P_value > 1:
+                raise ValueError('P should be in range [0, 1]')
+            return P_value
+        else:
+            return 0
+    except Exception as e:
+        print(f"Произошла ошибка: {e}")
+
+
+def longest_ones_sequence_test(sequence: str):
+    """
+    Функция принимает последовательность бит в виде строки,
+    а затем рассчитывает P-значение для теста на самую длинную
+    последовательность единиц в блоке и возвращает его.
+    :param sequence:  последовательность бит в виде строки(str).
+    :return float:
+    """
+    try:
+        blocks = []
+        for i in range(0, int(len(sequence) / LENGTH_OF_BLOCK)):
+            blocks.append(sequence[i * LENGTH_OF_BLOCK: (i + 1) * LENGTH_OF_BLOCK])
+        V = [0, 0, 0, 0]
+        for block in blocks:
+            count = 0
+            max_length = 0
+            for bit in block:
+                if bit == "1":
+                    count += 1
+                    max_length = max(max_length, count)
+                else:
+                    count = 0
+            if max_length <= 1:
+                V[0] += 1
+            if max_length == 2:
+                V[1] += 1
+            if max_length == 3:
+                V[2] += 1
+            if max_length >= 4:
+                V[3] += 1
+        Xi_in_2 = 0
+        for i in range(0, 4):
+            Xi_in_2 += pow((V[i] - 16 * PI_I[i]), 2) / (16 * PI_I[i])
+        P_value = gammainc(1.5, (Xi_in_2 / 2))
+        if P_value < 0 or P_value > 1:
+            raise ValueError('P should be in range [0, 1]')
+        return P_value
+    except Exception as e:
+        print(f"Произошла ошибка: {e}")
+
+
+def main() -> None:
+    """
+    Функция считывает из Json файла последовальности бит,
+    полученные из генераторов рандомных чисел C++, JAVA, Python.
+    Затем проводит тесты для каждого из них.
+    :param :
+    :return None:
+    """
+    try:
+        absolute_path = os.path.abspath(os.getcwd())
+        json_data = read_json_file(absolute_path + PATH)
+        if json_data:
+            CPP_SEQUENCE = json_data.get("CPP", "")
+            JAVA_SEQUENCE = json_data.get("JAVA", "")
+            PYTHON_SEQUENCE = json_data.get("PYTHON", "")
+        if CPP_SEQUENCE and JAVA_SEQUENCE and PYTHON_SEQUENCE:
+            print("Tests for CPP_SEQUENCE:")
+            print("Frequency bitwise test: P = " + str(frequency_bitwise_test(CPP_SEQUENCE)))
+            print("Similar sequences test: P = " + str(similar_sequences_test(CPP_SEQUENCE)))
+            print("Longest ones sequence test: P = " + str(longest_ones_sequence_test(CPP_SEQUENCE)))
+            print("Tests for JAVA_SEQUENCE:")
+            print("Frequency bitwise test: P = " + str(frequency_bitwise_test(JAVA_SEQUENCE)))
+            print("Similar sequences test: P = " + str(similar_sequences_test(JAVA_SEQUENCE)))
+            print("Longest ones sequence test: P = " + str(longest_ones_sequence_test(JAVA_SEQUENCE)))
+            print("Tests for PYTHON_SEQUENCE:")
+            print("Frequency bitwise test: P = " + str(frequency_bitwise_test(PYTHON_SEQUENCE)))
+            print("Similar sequences test: P = " + str(similar_sequences_test(PYTHON_SEQUENCE)))
+            print("Longest ones sequence test: P = " + str(longest_ones_sequence_test(PYTHON_SEQUENCE)))
+    except Exception as e:
+        print(f"Произошла ошибка: {e}")
 
 
 if __name__ == "__main__":
-    # main()
-    print(longest_sequence_test(
-        "11110001001000101111110101010100100000110011010110110111011101110001010111111001011110001101010010111000110001100110001100011111"))
+    main()
