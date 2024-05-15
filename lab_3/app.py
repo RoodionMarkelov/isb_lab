3a9dc6909ec6d3373b918bcac15d208d0afe78f3
rodio <rodionmarkelov01229@gmail.com>
2024-05-15 19:45:14 +0400

    start app

--- lab_3/app.py
+++ lab_3/app.py
@@ -16,60 +16,66 @@ from PyQt5.QtWidgets import (
 )
 from PyQt5.QtGui import QIcon, QFont
 
-from lab_3 import task
+import cryptosystem
 from constants import PATH
 
 
 class Window(QMainWindow):
     def __init__(self):
         super().__init__()
-
+        self.cryptosystem = None
         QToolTip.setFont(QFont("SansSerif", 14))
-
         self.setToolTip("Это <b>QWidget</b> виджет")
         self.label = QLabel(self)
         self.label.setText("Привет пользователь!")
         self.label.adjustSize()
         self.label.move(200, 50)
 
-        self.date = QLabel(self)
-        self.temp_morning = QLabel(self)
-        self.pres_morning = QLabel(self)
-        self.wind_morning = QLabel(self)
-        self.temp_evening = QLabel(self)
-        self.pres_evening = QLabel(self)
-        self.wind_evening = QLabel(self)
-
-        btn1 = QPushButton("Сгенерировать ключи", self)
+        btn1 = QPushButton("Инициализация криптосистемы по умолчанию", self)
         btn1.setToolTip(
-            "Нажмите на кнопку для генерации симмитричного, открытого и закрытого ключа."
+            "Нажмите на кнопку для создание файлов для ключей криптосистемы по умолчанию."
         )
-        btn1.clicked.connect(self._generate_keys)
+        btn1.clicked.connect(self.create_by_default)
         btn1.resize(btn1.sizeHint())
-        btn1.move(50, 50)
+        btn1.move(50, 100)
 
-        btn2 = QPushButton("Зашифровать данные", self)
+        btn2 = QPushButton("Инициализация криптосистемы пользователем", self)
         btn2.setToolTip(
-            "Нажмите на кнопку для шифрования данных."
+            "Нажмите на кнопку для выбора файлов для ключей криптосистемы."
         )
-        btn2.clicked.connect(self._encrypt)
+        btn2.clicked.connect(self.create_by_user)
         btn2.resize(btn2.sizeHint())
-        btn2.move(50, 100)
+        btn2.move(50, 150)
 
-        btn3 = QPushButton("Дешифровать данные", self)
+        btn3 = QPushButton("Создание ключей", self)
         btn3.setToolTip(
-            "Нажмите на кнопку для дешифрования данных."
+            "Нажмите на кнопку для создание ключей криптосистемы."
         )
-        btn3.clicked.connect(self._decrypt)
+        btn3.clicked.connect(self.generate_keys)
         btn3.resize(btn3.sizeHint())
-        btn3.move(50, 150)
+        btn3.move(50, 200)
+
+        btn4 = QPushButton("Зашифровать текст", self)
+        btn4.setToolTip(
+            "Нажмите на кнопку для шифрования текста."
+        )
+        btn4.clicked.connect(self.encrypt_text)
+        btn4.resize(btn4.sizeHint())
+        btn4.move(50, 250)
+
+        btn5 = QPushButton("Дешифровать текст", self)
+        btn5.setToolTip(
+            "Нажмите на кнопку для дешифрования текста."
+        )
+        btn5.clicked.connect(self.decrypt_text)
+        btn5.resize(btn4.sizeHint())
+        btn5.move(50, 300)
 
         qbtn = QPushButton("Выход", self)
         qbtn.setToolTip("Нажмите для кнопки для выхода.")
         qbtn.clicked.connect(self._quit)
         qbtn.resize(qbtn.sizeHint())
         qbtn.move(350, 400)
-
         self.resize(450, 450)
         self.center()
         self.setWindowTitle("app")
@@ -77,45 +83,35 @@ class Window(QMainWindow):
 
         self.show()
 
-    def _generate_keys(self):
-        json_data = self.read_json_file(os.path.abspath(os.getcwd()) + PATH)
-        if json_data:
-            symmetric_key = json_data.get("symmetric_key", "")
-            public_key = json_data.get("public_key", "")
-            secret_key = json_data.get("secret_key", "")
-        if symmetric_key and public_key and secret_key:
-            task.generate_keys(symmetric_key, public_key, secret_key, 16)
+    def center(self):
+        """Move the window to the center. Return None"""
+        location = self.frameGeometry()
+        center = QDesktopWidget().availableGeometry().center()
+        location.moveCenter(center)
+        self.move(location.topLeft())
 
-    def _encrypt(self):
-        json_data = self.read_json_file(os.path.abspath(os.getcwd()) + PATH)
-        if json_data:
-            symmetric_key = json_data.get("symmetric_key", "")
-            public_key = json_data.get("public_key", "")
-            secret_key = json_data.get("secret_key", "")
-        if symmetric_key and public_key and secret_key:
+    def get_file(self):
+        """Get file from directory"""
+        folderpath = QFileDialog.getExistingDirectory(self, "Выберете папку")
+        file_name = QFileDialog.getOpenFileName(
+            self, "Выберете файл", folderpath
+        )
+        return file_name[0]
 
-    def _decrypt(self):
+    def create_by_default(self):
         return
 
-    def read_json_file(file_path: str) -> dict:
-        """
-        Функция считывает данные из JSON файла.
-        :param file_path: указывает на расположение JSON файла.
-        :return dict:
-        """
-        try:
-            with open(file_path, "r", encoding="UTF-8") as file:
-                json_data = json.load(file)
-                return json_data
-        except FileNotFoundError:
-            print("Файл не найден.")
-            raise
-        except json.JSONDecodeError:
-            print("Ошибка при считывании JSON-данных.")
-            raise
-        except Exception as e:
-            print(f"Произошла ошибка: {e}")
-            raise
+    def create_by_user(self):
+        return
+
+    def generate_keys(self):
+        return
+
+    def encrypt_text(self):
+        return
+
+    def decrypt_text(self):
+        return
 
     def _quit(self):
         """Get MessageBox for exit"""
@@ -131,7 +127,8 @@ class Window(QMainWindow):
         else:
             return
 
-    if __name__ == "__main__":
-        app = QApplication(sys.argv)
-        window = Window()
-        sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = Window()
+    sys.exit(app.exec_())
