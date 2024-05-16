58cd5e79f6320ea20ce0c58fcb5a19bdfef24c25
rodio <rodionmarkelov01229@gmail.com>
2024-05-16 22:11:13 +0400

    final app

--- lab_3/app.py
+++ lab_3/app.py
@@ -3,7 +3,6 @@ import sys
 import os
 
 from PyQt5.QtWidgets import (
-    QWidget,
     QMainWindow,
     QToolTip,
     QPushButton,
@@ -11,8 +10,7 @@ from PyQt5.QtWidgets import (
     QDesktopWidget,
     QFileDialog,
     QLabel,
-    QMessageBox,
-    QCalendarWidget,
+    QMessageBox, QComboBox,
 )
 from PyQt5.QtGui import QIcon, QFont
 
@@ -20,16 +18,53 @@ import cryptosystem
 from constants import PATH
 
 
+def read_json_file(file_path: str) -> dict:
+    """
+    Функция считывает данные из JSON файла.
+    :param file_path: указывает на расположение JSON файла.
+    :return dict:
+    """
+    try:
+        with open(file_path, "r", encoding="UTF-8") as file:
+            json_data = json.load(file)
+            return json_data
+    except FileNotFoundError:
+        print("Файл не найден.")
+        raise
+    except json.JSONDecodeError:
+        print("Ошибка при считывании JSON-данных.")
+        raise
+    except Exception as e:
+        print(f"Произошла ошибка: {e}")
+        raise
+
+
 class Window(QMainWindow):
     def __init__(self):
         super().__init__()
+
+        self.text = None
+        self.encrypted_file = None
+        self.decrypted_file = None
+        self.symmetric_key = None
+        self.public_key = None
+        self.private_key = None
         self.cryptosystem = None
+
         QToolTip.setFont(QFont("SansSerif", 14))
         self.setToolTip("Это <b>QWidget</b> виджет")
-        self.label = QLabel(self)
-        self.label.setText("Привет пользователь!")
-        self.label.adjustSize()
-        self.label.move(200, 50)
+
+        self.messagelabel = QLabel(self)
+        self.messagelabel.setText("Привет пользователь!")
+        self.messagelabel.adjustSize()
+        self.messagelabel.move(200, 50)
+
+        self.label_number_of_bits = QLabel(self)
+        self.label_number_of_bits.setText("Количество битов:")
+        self.label_number_of_bits.adjustSize()
+        self.label_number_of_bits.move(290, 207)
+
+        self.UiComponents()
 
         btn1 = QPushButton("Инициализация криптосистемы по умолчанию", self)
         btn1.setToolTip(
@@ -51,7 +86,7 @@ class Window(QMainWindow):
         btn3.setToolTip(
             "Нажмите на кнопку для создание ключей криптосистемы."
         )
-        btn3.clicked.connect(self.generate_keys)
+        btn3.clicked.connect(self.generate_keys_for_cryptosystem)
         btn3.resize(btn3.sizeHint())
         btn3.move(50, 200)
 
@@ -76,7 +111,7 @@ class Window(QMainWindow):
         qbtn.clicked.connect(self._quit)
         qbtn.resize(qbtn.sizeHint())
         qbtn.move(350, 400)
-        self.resize(450, 450)
+        self.resize(500, 500)
         self.center()
         self.setWindowTitle("app")
         self.setWindowIcon(QIcon("web.png"))
@@ -99,19 +134,61 @@ class Window(QMainWindow):
         return file_name[0]
 
     def create_by_default(self):
-        return
+        absolute_path = os.path.abspath(os.getcwd())
+        json_data = read_json_file(absolute_path + PATH)
+        if json_data:
+            text = json_data.get("text", "")
+            encrypted_file = json_data.get("encrypted_file", "")
+            decrypted_file = json_data.get("decrypted_file", "")
+            symmetric_key = json_data.get("symmetric_key", "")
+            public_key = json_data.get("public_key", "")
+            private_key = json_data.get("private_key", "")
+        if text and encrypted_file and decrypted_file and symmetric_key and private_key and public_key :
+            self.text = absolute_path + text
+            self.encrypted_file = absolute_path + encrypted_file
+            self.decrypted_file = absolute_path + decrypted_file
+            self.symmetric_key = absolute_path + symmetric_key
+            self.public_key = absolute_path + public_key
+            self.private_key = absolute_path + private_key
+            number_of_bites = int(self.find())
+            self.cryptosystem = cryptosystem.Cryptosystem(number_of_bites)
+
+    def UiComponents(self):
+        self.combo_box = QComboBox(self)
+
+        self.combo_box.setGeometry(400, 200, 50, 30)
+
+        list_of_number_of_bits = ["64", "128", "192"]
+
+        self.combo_box.addItems(list_of_number_of_bits)
+
+    def find(self):
+
+        content = self.combo_box.currentText()
+
+        self.label.setText("Content : " + content)
 
     def create_by_user(self):
-        return
+        self.text = self.get_file()
+        self.encrypted_file = self.get_file()
+        self.decrypted_file = self.get_file()
+        self.symmetric_key = self.get_file()
+        self.public_key = self.get_file()
+        self.private_key = self.get_file()
+        number_of_bites = int(self.find())
+        self.cryptosystem = cryptosystem.Cryptosystem(number_of_bites)
 
-    def generate_keys(self):
-        return
+    def generate_keys_for_cryptosystem(self):
+        self.cryptosystem.generate_keys(self.symmetric_key, self.public_key, self.private_key)
+        self.messagelabel.setText("Ключи созданы.")
 
     def encrypt_text(self):
-        return
+        self.cryptosystem.encrypt(self.text, self.symmetric_key, self.private_key, self.encrypted_file)
+        self.messagelabel.setText("Текст зашифрован.")
 
     def decrypt_text(self):
-        return
+        self.cryptosystem.decrypt(self.encrypted_file, self.symmetric_key, self.private_key, self.decrypted_file)
+        self.messagelabel.setText("Текст дешифрован.")
 
     def _quit(self):
         """Get MessageBox for exit"""
--- lab_3/asymmetric_key.py
+++ lab_3/asymmetric_key.py
@@ -6,25 +6,21 @@ from cryptography.hazmat.primitives.serialization import load_pem_public_key, lo
 
 
 class AsymmetricKey:
-    def __init__(self, number_of_bites):
-        self.number_of_bites = number_of_bites
-        self.public_key = None
-        self.private_key = None
 
-    def serialization_private(self, path_to_private_key):
+    def serialization_private(self, path_to_private_key, private_key):
         with open(path_to_private_key, 'wb') as private_out:
-            private_out.write(self.private_key.private_bytes(encoding=serialization.Encoding.PEM,
+            private_out.write(private_key.private_bytes(encoding=serialization.Encoding.PEM,
                                                              format=serialization.PrivateFormat.TraditionalOpenSSL,
                                                              encryption_algorithm=serialization.NoEncryption()))
 
-    def serialization_public(self, path_to_public_key):
+    def serialization_public(self, path_to_public_key, public_key):
         with open(path_to_public_key, 'wb') as public_out:
-            public_out.write(self.public_key.public_bytes(encoding=serialization.Encoding.PEM,
+            public_out.write(public_key.public_bytes(encoding=serialization.Encoding.PEM,
                                                           format=serialization.PublicFormat.SubjectPublicKeyInfo))
 
-    def serialization_keys(self, path_to_private_key, path_to_public_key):
-        self.serialization_private(path_to_private_key)
-        self.serialization_public(path_to_public_key)
+    def serialization_keys(self, path_to_private_key, path_to_public_key, private_key, public_key):
+        self.serialization_private(path_to_private_key, private_key)
+        self.serialization_public(path_to_public_key, public_key)
 
     def deserialization_private(self, path_to_private_key):
         with open(path_to_private_key, 'rb') as pem_in:
@@ -43,18 +39,20 @@ class AsymmetricKey:
             public_exponent=65537,
             key_size=2048
         )
-        self.private_key = keys
-        self.public_key = keys.public_key()
+        private_key = keys
+        public_key = keys.public_key()
+        return private_key, public_key
 
-    def encrypt_text(self, text):
-        encrypted_text = self.public_key.encrypt(text,
+    def encrypt_text(self, text, public_key):
+        encrypted_text = public_key.encrypt(text,
                                                  padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()),
                                                               algorithm=hashes.SHA256(),
                                                               label=None))
         return encrypted_text
 
-    def decrypt_text(self, encrypted_text):
-        decrypted_text = self.private_key.decrypt(encrypted_text,
+    def decrypt_text(self, path_to_private_key, encrypted_text):
+        private_key = self.deserialization_private(path_to_private_key)
+        decrypted_text = private_key.decrypt(encrypted_text,
                                                   padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()),
                                                                algorithm=hashes.SHA256(), label=None))
         return decrypted_text
